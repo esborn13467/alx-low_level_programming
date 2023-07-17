20d06ba3703d78d3437e7aac3796daef1c6679f4
esborn13467 <esbornchiira8@gmail.com>
2023-07-17 13:09:56 -0700

    Initial commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - entry point
+*return: always 0 (success)
+*/
+int main(void)
+/**
+* return: always 0 (success)
+*/	
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
